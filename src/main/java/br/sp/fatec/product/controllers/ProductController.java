750b2470913d02b86e3a74dbb3f253cf201bb415
William Estevam <estevamwilliam16@gmail.com>
2025-10-21 11:12:20 -0300

    :sparkles: feat: Minimal mapping, repository creation, database connection, Controller modification.

--- src/main/java/br/sp/fatec/product/controllers/ProductController.java
+++ src/main/java/br/sp/fatec/product/controllers/ProductController.java
@@ -1,40 +1,26 @@
 package br.sp.fatec.product.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.sp.fatec.product.entities.Product;
-import jakarta.persistence.EntityNotFoundException;
+import br.sp.fatec.product.repositories.ProductRepository;
 
 @RestController
 @RequestMapping("products")
 
 public class ProductController { //cria o controller
 
-    private ArrayList<Product> list = new ArrayList<Product>(); //cria uma lista
-
-    public ProductController() { //cria os produtos
-        list.add(new Product(1L, "Produto 1", "Descrição 1", 10.0));
-        list.add(new Product(2L, "Produto 2", "Descrição 2", 20.0));
-        list.add(new Product(3L, "Produto 3", "Descrição 3", 30.0));
-    }
+    @Autowired
+    private ProductRepository repository;
     @GetMapping 
-    public List<Product> getProducts () //retorna a lista
+    public List<Product> getProducts () //devolve todos produtos
         {
-            return list;
+            return repository.findAll();
         }
-
-    @GetMapping("{id}")
-        public Product getProductById(@PathVariable long id) {
-            return list.stream()
-                 .filter( p -> p.getId() == id) //procura o id no arry e mostra na tela
-                 .findFirst()
-                 .orElseThrow( () -> new EntityNotFoundException("Não cadastrado")); //erro quando não achar nenhum id
-    }
 
 }
--- src/main/java/br/sp/fatec/product/entities/Product.java
+++ src/main/java/br/sp/fatec/product/entities/Product.java
@@ -1,10 +1,25 @@
 package br.sp.fatec.product.entities;
 
-public class Product {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "TBL_PRODUCT")
+public class Product implements Serializable {
+    
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
     private Double price;
+
+    public Product() {}
 
     public Product(Long id, String name, String description, Double price) {
         this.id = id;
@@ -36,4 +51,29 @@ public class Product {
     public void setPrice(Double price) {
         this.price = price;
     }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
 }
