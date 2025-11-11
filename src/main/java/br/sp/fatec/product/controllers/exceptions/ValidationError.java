8e880c422dffb205591ea9f2490e087c121da829
William Estevam <estevamwilliam16@gmail.com>
2025-11-11 11:12:10 -0300

    :sparkles: feat: Exception Handler Implementation.

---
+++ src/main/java/br/sp/fatec/product/controllers/exceptions/ValidationError.java
@@ -0,0 +1,17 @@
+package br.sp.fatec.product.controllers.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardError {
+
+    private List<String> errors = new ArrayList<>();
+
+    public void addError(String error){
+        this.errors.add(error);
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
